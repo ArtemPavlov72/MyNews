ae455acbb73806bcb89c638c9bcce0a862f03c6a
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-08-22 16:00:40 +0500

    Initial commit

---
+++ MyNews/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MyNews
+//
+//  Created by Artem Pavlov on 22.08.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    window?.rootViewController = UINavigationController(rootViewController: NewsListViewController())
+    return true
+  }
+}
---
+++ MyNews/ViewControllers + ViewModels/NewsListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsListViewController.swift
+//  MyNews
+//
+//  Created by Artem Pavlov on 22.08.2023.
+//
+
+import UIKit
+
+class NewsListViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+
+
+}
+
