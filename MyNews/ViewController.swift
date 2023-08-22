da002598ff02a2a7ee4150ef3d6b02ce8a39a116
ArtemPavlov72 <pavlov.tema@gmail.com>
2023-08-22 15:31:45 +0500

    Initial Commit

---
+++ MyNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNews
+//
+//  Created by Артем Павлов on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
